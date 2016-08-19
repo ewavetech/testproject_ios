41549aa85d50a2cc7a576bca61ea97c74156c99d
Adil Sher <adilsher7@gmail.com>
2016-08-19 18:30:20 +0500

    Initial Commit

---
+++ github_test_project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  github_test_project
+//
+//  Created by Adil Sher on 8/19/16.
+//  Copyright © 2016 Adil Sher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
